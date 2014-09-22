9d0fd0d33441b7976d2265c548bc81c34a1a4400
Curtis Carter <curtis@rubyhq.com>
2014-09-22 12:39:56 -0500

    Added Scotty dependency and simple route example

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty
+
 main :: IO ()
-main = putStrLn "wee"
+main = scotty 3000 $ do
+         get "/:word" $ do
+           word <- param "word"
+           text word
